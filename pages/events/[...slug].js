ca163822410c4b38371580d574e3c148fcb7b3ec
HyeonJu <hhjc017@gmail.com>
2022-10-08 20:27:36 +0900

    filtered event page 컴포넌트 분리

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,25 +1,16 @@
 import { useRouter } from 'next/router'
+import EventList from '../../components/events/event-list'
 import { getEventsByDate } from '../../dummy-data'
 // FilterdEventsPage
 
 export default function EventSlugPage() {
   const router = useRouter()
   const { slug } = router.query
-  const selectedYear = slug[0]
-  const selectedMonth = slug[1]
+  const [selectedYear, selectedMonth] = slug
+  const eventList = getEventsByDate({
+    month: selectedMonth,
+    year: selectedYear,
+  })
 
-  return (
-    <ul>
-      {getEventsByDate({ month: selectedMonth, year: selectedYear }) //
-        ?.map(({ id, title, date, location, description }) => (
-          <section key={`filterd-${id}`}>
-            <h1>{title}</h1>
-            <p>
-              on {date}, in {location}
-            </p>
-            <p>{description}</p>
-          </section>
-        ))}
-    </ul>
-  )
+  return <EventList eventlist={eventList} />
 }

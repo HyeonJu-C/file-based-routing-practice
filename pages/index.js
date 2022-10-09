4f5290951545fa4d51edae89a5246accd1f47cdc
HyeonJu <hhjc017@gmail.com>
2022-10-09 09:29:06 +0900

    layout 추가

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
 import Link from 'next/link'
 import EventList from '../components/events/event-list'
-import Header from '../components/header'
 import { getFeaturedEvents } from '../dummy-data'
 
 export default function HomePage() {
   const featuredEventList = getFeaturedEvents()
-  return (
-    <>
-      <Header />
-      <EventList eventlist={featuredEventList} />
-    </>
-  )
+  return <EventList eventlist={featuredEventList} />
 }

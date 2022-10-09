f1e2df2b2c564acd39da1ff5d85c6602d320bd41
HyeonJu <hhjc017@gmail.com>
2022-10-09 11:42:44 +0900

    slug page에서 url 데이터가 undefined 일 때 분기 처리

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -6,11 +6,20 @@ import { getEventsByDate } from '../../dummy-data'
 export default function EventSlugPage() {
   const router = useRouter()
   const { slug } = router.query
+
+  // first rendering
+  if (!slug) return <p>loading...</p>
+
+  // second rendering
   const [selectedYear, selectedMonth] = slug
+
   const eventList = getEventsByDate({
     month: selectedMonth,
     year: selectedYear,
   })
+
+  // +) selectedYear, selectedMonth가 유효하지 않을 때 분기 처리
+  // +) 값은 유효하지만 eventList의 length가 0일 때 분기 처리
 
   return <EventList eventlist={eventList} />
 }
